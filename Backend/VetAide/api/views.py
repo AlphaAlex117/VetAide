7b6df6eb20706bc0253b218d503a2922c917fc50
Alexander Lafontaine <71290520+AlphaAlex117@users.noreply.github.com>
2024-02-17 22:32:40 -0600

    adding basic get and post requests

--- Backend/VetAide/api/views.py
+++ Backend/VetAide/api/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+import json
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+### GET ALL schedules
+def get_all_schedules(request):
+    return None
+
+### GET ALL inventory
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+### GET all animals
+
+### GET an animal's information
+def animal_information(request):
+    name = request.GET.get("name")
+    
+    return JsonResponse({'name':'None'})
+
+### GET all medical records of an animal
+
+
+### POST appointement
+def make_appointment(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    patient_name = body["patientName"]
+    owner_name = body["owner"]
+    vet_name = body["veterinarian"]
+    date = body["appointmentDate"]
+    reason = body["operationCategory"]
+
+    
+
+    return JsonResponse({'success':True})
+
+### POST
+
+### POST
